6017f6bffb4115745fb62018595e30f058544a13
dragonmux <git@dragonmux.network>
2023-04-30 00:07:47 +0100

    misc: Created a build system for the project and a stub main implementation

---
+++ src/bmpflash.cxx
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: 2023 1BitSquared <info@1bitsquared.com>
+// SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
+
+int main(int, char **)
+{
+}
