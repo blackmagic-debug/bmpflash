88161ace613383871f911e485f22a3f690c77d47
dragonmux <git@dragonmux.network>
2023-05-04 17:43:22 -0400

    unicode: Fixed our not including cstdint, breaking the build on GCC 13

--- src/unicode.cxx
+++ src/unicode.cxx
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // SPDX-FileCopyrightText: 2023 1BitSquared <info@1bitsquared.com>
 // SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
+#include <cstdint>
 #include <cstring>
 #include <limits>
 #include <type_traits>
