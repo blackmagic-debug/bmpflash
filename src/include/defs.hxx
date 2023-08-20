3f14f0b1b4c2dd6fa8d59d5d94cfa02fe84a5085
dragonmux <git@dragonmux.network>
2023-08-20 23:02:08 +0100

    usbTypes: Fixed the MSVC warning about not knowing the packing attribute used

---
+++ src/include/defs.hxx
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: 2023 1BitSquared <info@1bitsquared.com>
+// SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
+#ifndef DEFS_HXX
+#define DEFS_HXX
+
+#if defined(__has_include) && __has_include(<version>)
+#include <version>
+#endif
+
+#if (defined(__has_cpp_attribute) && __has_cpp_attribute(gnu::packed)) || defined(__GNUC__)
+#define BEGIN_PACKED(n)
+#define END_PACKED()
+#define ATTR_PACKED [[gnu::packed]]
+#elif defined(_MSC_VER)
+#define STRINGIFY(n) #n
+#define BEGIN_PACKED(n) _Pragma("push(pack, " STRINGIFY(n) ")")
+#define END_PACKED() _Pragma("pop(pack)")
+#define ATTR_PACKED
+#endif
+
+#endif /*DEFS_HXX*/
