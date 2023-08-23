672f7cf4c8247ef5c265decffc2b59dd65e259f2
dragonmux <git@dragonmux.network>
2023-08-23 04:53:47 +0100

    flashVendors: Implemented a UDL to solve the conversions warnings for the flashVendors array

--- src/include/flashVendors.hxx
+++ src/include/flashVendors.hxx
@@ -10,12 +10,15 @@
 
 using namespace std::literals::string_view_literals;
 
+constexpr inline uint8_t operator ""_u8(const unsigned long long value)
+	{ return static_cast<uint8_t>(value); }
+
 static const std::map<uint8_t, std::string_view> flashVendors
 {
-	{0x1fU, "Adesto"sv},
-	{0x20U, "Numonyx"sv},
-	{0xc8U, "GigaDevice"sv},
-	{0xefU, "Winbond"sv},
+	{0x1f_u8, "Adesto"sv},
+	{0x20_u8, "Numonyx"sv},
+	{0xc8_u8, "GigaDevice"sv},
+	{0xef_u8, "Winbond"sv},
 };
 
 #endif /*FLASH_VENDORS_HXX*/
